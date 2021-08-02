92457ee25566a399aa5358129f6079f25fe6560e
seoin <jnh02215@naver.com>
2021-08-02 17:44:35 +0900

    '1:1'

---
+++ src/main/java/com/bbangduck/BbangduckSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bbangduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangduckSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbangduckSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbangduck/BbangduckSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbangduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangduckSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
